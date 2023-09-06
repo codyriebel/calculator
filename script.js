1851f6b7df90ee2be91168647e5557dc8b0c321b
Cody Riebel <cody.riebel@gmail.com>
2023-09-05 22:46:20 -0400

    in progress: re-work structure to handle negative numbers

--- script.js
+++ script.js
@@ -5,11 +5,36 @@ const divide = (a, b) => a / b;
 
 let numOne = 0;
 let numTwo = 0;
+let operator = '';
 let numOperators = 0;
+let numMinus = 0;
+
+let display = document.querySelector('#display');
+
+let buttons = document.querySelectorAll('button');
+for (button of buttons) {
+  button.addEventListener('click', btnPress);
+}
+
+let writeDisplay = value => display.textContent += value;
+
+let clearDisplay = () => {
+  display.textContent = '';
+  numOne = 0;
+  numTwo = 0;
+  operator = '';
+  numOperators = 0;
+  numMinus = 0;
+};
+
+let writeHistory = ans => {
+  let expression = display.textContent;
+  document.querySelector('#history').textContent = `${expression} = ${ans}`;
+};
 
 let operate = (a, b, operator) => {
   if (operator == '+') return add(a, b);
-  else if (operator == '-') return subtract(a, b);
+  else if (operator == 'minus') return subtract(a, b);
   else if (operator == 'x') return multiply(a, b);
   else if (operator == '/') {
     if (b == 0) return 'zero division';
@@ -17,75 +42,73 @@ let operate = (a, b, operator) => {
   }
 };
 
-let getAnswer = (dv) => {
-  [numOne, numTwo] = dv.split(/[\+\-\x\/]/);
-  let oprMatch = dv.match(/[\+\-\x\/]/);
-
-  if (oprMatch) {
-    let opr = oprMatch[0];
-    let answer = operate(parseInt(numOne), parseInt(numTwo), opr);
+let getAnswer = () => {
+    let answer = operate(parseInt(numOne), parseInt(numTwo), operator);
 
     if (answer == 'zero division') return 'nice try...';
     if (!(isNaN(answer) || answer == null)) {
-      return Math.round(answer *  100) / 100;
+      return Math.round(answer *  1000) / 1000;
     }
-  }
   return '';
 };
 
-let btnPress = btn => {
+function btnPress(btn) {
   let value = btn.target.textContent;
-  if (value == 'CLEAR') return clearDisplay();
+  let dv = display.textContent
 
-  let dv = getDisplayValue();
-  if (value == 'DEL') return backspace(dv);
+  numOperators = dv.search(/[\+\x\/]/g).length;
 
-  // if (value == '(-)') return negative(dv);
-  
-  if (btn.target.classList.contains('operator')) {
+  if (value == 'CLEAR') {
+    clearDisplay();
+
+  } else if (value == 'DEL') {
+    display.textContent = dv.slice(0, -1); 
+
+  } else if (value == '-') {
+    // if first or second number neg
+    if (dv == '' || numOperators + numMinus > 0) return;
+    // if operator minus
+    else {
+      numOne = dv;
+      numMinus += 1;
+      operator = 'minus';
+    }
+
+    // if other operator
+  } else if (
+    value.search(/[\+\x\/]/) &&
+    numOperators == 0
+  ) {
+    operator = value;
     numOperators += 1;
-  }
 
-  if (value == '=' || numOperators == 2) {
-    let ans = getAnswer(dv);
+  }
+  
+  if (value == '=' || numOperators + numMinus == 2) {
+    if (numMinus > 0) {
+      numTwo = display.textContent.split('-')[1];
+    } else {
+      numTwo = display.textContent.split(operator)[1];
+    }
+    
+    let ans = getAnswer();
 
     writeHistory(ans);
     clearDisplay();
     writeDisplay(ans);
 
+    // if other operator
     if (value != '=') {
       writeDisplay(value);
+      operator = value;
       numOperators += 1;
     } 
-  } else {
+    return;
+  }
+  
+  if (!(btn.target.classList.contains('fcn'))) {
     writeDisplay(value);
-  } 
+  }
+
+  
 };
-
-let handleError = () => {
-  clearDisplay();
-  writeHistory('ERROR');
-};
-
-let writeDisplay = value => document.querySelector('#display').textContent += value;
-
-let clearDisplay = () => {
-  document.querySelector('#display').textContent = '';
-  numOperators = 0;
-};
-
-let backspace = (dv) => {
-  document.querySelector('#display').textContent = dv.slice(0, -1);
-}
-
-let writeHistory = ans => {
-  let expression = document.querySelector('#display').textContent;
-  document.querySelector('#history').textContent = `${expression} = ${ans}`;
-};
-
-let getDisplayValue = () => document.querySelector('#display').textContent;
-
-let buttons = document.querySelectorAll('button');
-for (button of buttons) {
-  button.addEventListener('click', btnPress);
-}
